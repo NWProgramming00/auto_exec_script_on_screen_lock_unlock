191974b84b0258be2ab9978c5e5d9d3871088a5c
NWProgramming00 <75951308+NWProgramming00@users.noreply.github.com>
2021-06-15 13:08:44 +0200

    auto_exec_lul.sh Script to automated command execution.
    This script was written for Arch Linux.
    As far as i know It should work flawlessly on any debian-based system.
    Other Linux systems were not checked.

---
+++ auto_exec_lul.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# gdbus monitor is running in constant loop monitoring bus events
+gdbus monitor -y -d org.freedesktop.login1 |
+  while read x; do
+    case "$x" in
+    # REPLACE PATH WITH YOUR PATH :)
+      *"true"*) PATH/TO/YOUR/LOCK/SCREEN/SCRIPT;;
+      *"false"*) PATH/TO/YOUR/UNLOCK/SCREEN/SCRIPT;;
+    esac
+  done
