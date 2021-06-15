32ed1c7bde6e063e5660f183bed50efbd957f0f8
NWProgramming00 <75951308+NWProgramming00@users.noreply.github.com>
2021-06-15 14:19:46 +0200

    Update auto_exec_on_lockvsunlock.sh

--- auto_exec_on_lockvsunlock.sh
+++ auto_exec_on_lockvsunlock.sh
@@ -2,10 +2,10 @@
 
 # gdbus monitor is running in constant loop monitoring bus events
 gdbus monitor -y -d org.freedesktop.login1 |
-  while read x; do
-    case "$x" in
-    # REPLACE PATH WITH YOUR PATH :)
-      *"true"*) PATH/TO/YOUR/LOCK/SCREEN/SCRIPT;;
-      *"false"*) PATH/TO/YOUR/UNLOCK/SCREEN/SCRIPT;;
-    esac
-  done
+	while read x; do
+	case "$x" in
+		# REPLACE PATH WITH YOUR PATH :)
+		*"true"*) PATH/TO/YOUR/LOCK/SCREEN/SCRIPT;;
+      	*"false"*) PATH/TO/YOUR/UNLOCK/SCREEN/SCRIPT;;
+		esac
+	done
